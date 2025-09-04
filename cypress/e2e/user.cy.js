d68851a36dc259f53abe69a602c83a892a7a1087
Mayara <mayaramoreira.msr@gmail.com>
2025-09-04 17:08:23 -0300

    add page objects

--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -1,38 +1,45 @@
 import userData from "../fixtures/user-data.json";
+import LoginPage from "../pages/loginPage.js";
+import DashboardPage from "../pages/dashboardPage.js";
+import MenuPage from "../pages/menuPage.js";
+
+const loginPage = new LoginPage();
+const dashboardPage = new DashboardPage();
+const menuPage = new MenuPage();
 
 describe("testes no ORANGE HRM", () => {
   const selectorsList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton: ".oxd-button",
-    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb > .oxd-text",
-    dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: ".oxd-alert-content",
-    myInfoButton: '[href = "/web/index.php/pim/viewMyDetails"]',
     firstNameField: "[name='firstName']",
     lastNameField: "[name = 'lastName']",
     genericField: ".oxd-input--active",
-    dateField: "[placeholder='dd-mm-yyyy']",
+    dateField: ".oxd-input",
+    genericComboBox: ".oxd-select-text-input",
     dateCloseButton: ".--close",
     submitButton: "[type = 'submit']",
   };
 
   it.only("user info update - Success!", () => {
-    cy.visit("/auth/login");
-    cy.get(selectorsList.usernameField).type(userData.userSuccess.username);
-    cy.get(selectorsList.passwordField).type(userData.userSuccess.password);
-    cy.get(selectorsList.loginButton).click();
-    cy.get(selectorsList.sectionTitleTopBar).contains("Dashboard");
-    cy.get(selectorsList.myInfoButton).click();
+    loginPage.accessLoginPage();
+    loginPage.loginWithAnyUser(
+      userData.userSuccess.username,
+      userData.userSuccess.password
+    );
+    dashboardPage.checkDashboardPage();
+    menuPage.accessMyInfo();
+
     cy.get(selectorsList.firstNameField).clear().type("firstNameTest");
     cy.get(selectorsList.lastNameField).clear().type("lastNameTest");
     cy.get(selectorsList.genericField).eq(3).clear().type("EmployeeID");
     cy.get(selectorsList.genericField).eq(4).clear().type("othertest");
     cy.get(selectorsList.genericField).eq(5).clear().type("Drivertest");
-    cy.get(selectorsList.dateField).eq(0).clear().type("2026-10-03");
+    cy.get(selectorsList.dateField).eq(7).clear().type("2026-10-03");
     cy.get(selectorsList.dateCloseButton).click();
-    cy.get(selectorsList.submitButton).eq(0).click();
+    cy.get(selectorsList.submitButton).eq(0).click({ force: true });
     cy.get("body").should("contain", "Successfully Updated");
+    cy.get(selectorsList.genericComboBox).eq(0).click();
+    cy.get(".oxd-select-dropdown > :nth-child(4)").click();
+    cy.get(selectorsList.genericComboBox).eq(1).click();
+    cy.get(".oxd-select-dropdown > :nth-child(2) > span").click();
   });
 
   it("login - Fail!", () => {
